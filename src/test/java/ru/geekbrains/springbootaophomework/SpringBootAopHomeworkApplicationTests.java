1aad5b728f771270c33cd1d9bfdf785aaacf2d31
AleksOstr <ostryankoam@yandex.ru>
2024-02-06 22:47:39 +0400

    Add Spring Boot Starter AOP dependency

--- src/test/java/ru/geekbrains/springbootaophomework/SpringBootAopHomeworkApplicationTests.java
+++ src/test/java/ru/geekbrains/springbootaophomework/SpringBootAopHomeworkApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootAopHomeworkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
